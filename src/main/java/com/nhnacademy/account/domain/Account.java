2f7bff046a3d92b715594e21259503a6766ec7ad
Duwing <ssuk0406@gmail.com>
2026-07-10 13:22:56 +0900

    feat : account 엔티티 JPA 어노테이션 추가 및 정책 구체화

--- src/main/java/com/nhnacademy/account/domain/Account.java
+++ src/main/java/com/nhnacademy/account/domain/Account.java
@@ -5,18 +5,20 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
+import java.util.UUID;
 
 @Entity
 @Getter
 @NoArgsConstructor
+@Table(name = "account")
 public class Account {
 
     // 계정 생성
-    public Account(Long id, String username, String password, String email) {
-        this.id = id;
-        this.username = username;
-        this.password = password;
+    public Account(String username, String hashedPassword, String email) {
+        this.uuid =  UUID.randomUUID();
         this.email = email;
+        this.hashedPassword = hashedPassword;
+        this.username = username;
 
         accountStatus = AccountStatus.ACTIVE;
         createdAt = LocalDate.now();
@@ -24,28 +26,42 @@ public class Account {
     }
 
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    // DB 식별자
     private Long id;
 
-    private String username;
+    @Column(name = "uuid", unique = true, nullable = false)
+    // jwt sub
+    private UUID uuid;
 
-    private String password;
-
+    @Column(name = "email", unique = true)
     private String email;
 
+    @Column(name = "password_hash")
+    private String hashedPassword;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "account_status")
+    @Enumerated(EnumType.STRING)
     private AccountStatus accountStatus;
 
+    @Column(name = "created_at")
     private LocalDate createdAt;
 
+    @Column(name = "updated_at")
     private LocalDate updatedAt;
 
+    @Column(name = "withdrawn_at")
     private LocalDate withdrawnAt;
 
 
-    public void updateAccount(String username, String password, String email) {
-        this.username = username;
-        this.password = password;
+    public void updateAccount(String email, String hashedPassword, String username) {
         this.email = email;
+        this.hashedPassword = hashedPassword;
+        this.username = username;
+
         this.updatedAt = LocalDate.now();
     }
 
@@ -55,7 +71,7 @@ public class Account {
 
         if (accountStatus == AccountStatus.WITHDRAWN) {
             this.username = null;
-            this.password = null;
+            this.hashedPassword = null;
             this.email = null;
             this.withdrawnAt = LocalDate.now();
         }
