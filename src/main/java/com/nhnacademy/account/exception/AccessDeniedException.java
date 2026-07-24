2063d79cc24976ab97026890e459677dc86e4112
Duwing <ssuk0406@gmail.com>
2026-07-24 09:37:51 +0900

    feat : 인가 예외 추가

---
+++ src/main/java/com/nhnacademy/account/exception/AccessDeniedException.java
@@ -0,0 +1,11 @@
+package com.nhnacademy.account.exception;
+
+import com.nhnacademy.account.global.error.ErrorCode;
+import com.nhnacademy.account.global.error.exception.BaseException;
+
+public class AccessDeniedException extends BaseException {
+
+    public AccessDeniedException() {
+        super(ErrorCode.ACCESS_DENIED);
+    }
+}
