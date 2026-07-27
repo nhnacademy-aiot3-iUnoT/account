b5b6290c48275a8772ed6621d728faf7950c2200
Duwing <ssuk0406@gmail.com>
2026-07-27 18:03:26 +0900

    feat : email 중복 검사 관련 설정

--- src/main/java/com/nhnacademy/account/config/SecurityConfig.java
+++ src/main/java/com/nhnacademy/account/config/SecurityConfig.java
@@ -48,7 +48,7 @@ public class SecurityConfig {
 
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(
-                                "/api/auth/**", "/.well-known/jwks.json"
+                                "/api/auth/**", "/api/accounts/email", "/.well-known/jwks.json"
                         ).permitAll()
 
                         .requestMatchers(
