ab1513a9c920841f9b4f762b08bedd1e216ff25e
Duwing <ssuk0406@gmail.com>
2026-08-10 15:57:20 +0900

    feat: 엔티티의 테이블 명 수정

--- src/main/java/com/nhnacademy/account/domain/Account.java
+++ src/main/java/com/nhnacademy/account/domain/Account.java
@@ -15,7 +15,19 @@ import java.util.UUID;
 
 @Entity
 @Getter
-@Table(name = "account")
+@Table(
+        name = "accounts",
+        uniqueConstraints = {
+                @UniqueConstraint(
+                        name = "uk_account_uuid",
+                        columnNames = "uuid"
+                ),
+                @UniqueConstraint(
+                        name = "uk_account_email",
+                        columnNames = "email"
+                )
+        }
+)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Account {
 
@@ -31,7 +43,6 @@ public class Account {
     @Column(
             name = "uuid",
             nullable = false,
-            unique = true,
             updatable = false,
             columnDefinition = "BINARY(16)"
     )
@@ -39,7 +50,6 @@ public class Account {
 
     @Column(
             name = "email",
-            unique = true,
             nullable = false,
             length = 255
     )
@@ -59,6 +69,7 @@ public class Account {
     private String name;
 
     @Enumerated(EnumType.STRING)
+    @JdbcTypeCode(SqlTypes.VARCHAR)
     @Column(
             name = "account_status",
             nullable = false,
@@ -67,6 +78,7 @@ public class Account {
     private AccountStatus accountStatus;
 
     @Enumerated(EnumType.STRING)
+    @JdbcTypeCode(SqlTypes.VARCHAR)
     @Column(
             name = "account_role",
             nullable = false,
