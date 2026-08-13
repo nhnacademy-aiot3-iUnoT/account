2694b07b71ff6baffa2cce1f143f09e61c0dcade
Duwing <ssuk0406@gmail.com>
2026-08-13 18:09:51 +0900

    feat: jwks uri 수정

--- src/main/java/com/nhnacademy/account/controller/JwkSetController.java
+++ src/main/java/com/nhnacademy/account/controller/JwkSetController.java
@@ -3,12 +3,14 @@ package com.nhnacademy.account.controller;
 import com.nimbusds.jose.jwk.JWKSet;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/auth")
 public class JwkSetController {
     private final JWKSet jwkSet;
 
